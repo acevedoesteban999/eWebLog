fa5dc5cca8675b277c850381f961d236aa416d02
Esteban <acevedoesteban999@gmail.com>
2025-01-28 12:55:10 -0500

    ADD: EWEBLOG_CHECK_INIT Macro ADD: eweblog_add_format

--- eWebLog.c
+++ eWebLog.c
@@ -9,29 +9,60 @@ void eweblog_init(unsigned CAPACITY){
 }
 
 void eweblog_realocate(unsigned CAPACITY){
+    EWEBLOG_CHECK_INIT();
     estr_prepare_str(&EWEBLOG_STR,CAPACITY);
 }
 
 void eweblog_clear(){
+    EWEBLOG_CHECK_INIT();
     estr_clear(&EWEBLOG_STR);
 }
 
 void eweblog_free(){
+    EWEBLOG_CHECK_INIT();
     estr_free(&EWEBLOG_STR);
 }
 
 void eweblog_add_str(eSTR*str){
     eweblog_add_buff(str->ptr_char,str->length);
 }
+
 void eweblog_add_buff(char* buff, unsigned len) {
+    EWEBLOG_CHECK_INIT();
     if (EWEBLOG_STR.length + len > EWEBLOG_STR.capacity)
         estr_clear(&EWEBLOG_STR);
     estr_append_str(&EWEBLOG_STR, false, buff);
 }
 
 
+void eweblog_add_format(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    int size = vsnprintf(NULL, 0, format, args);
+    if (size < 0) {
+        va_end(args);
+        return ;
+    }
+
+    char *buff = (char *)malloc(size + 1); 
+    if (!buff) {
+        va_end(args);
+        return ; 
+    }
+
+    vsnprintf(buff, size + 1, format, args);
+    va_end(args);
+    
+    eweblog_add_buff(buff,size);
+    free(buff);
+}
+
+
 esp_err_t eweblog_get_post_handler(httpd_req_t *req){
+    EWEBLOG_CHECK_INIT();
     httpd_resp_set_type(req, "text/html");
     eweb_send_resp_try_chunk_str(req, &EWEBLOG_STR);
     return ESP_OK;
 }
+
--- include/eWebLog.h
+++ include/eWebLog.h
@@ -4,12 +4,19 @@
 
 #define EWEBLOG_DEFAULT_CAPACITY 1024
 
-
-#define EWEBLOG_HANDLERS( execution_funtion ) \
+#define EWEBLOG_HANDLERS(execution_funtion) \
     {{"/weblog_get", HTTP_POST , execution_funtion , NULL}, true, {.uri_execution_function = eweblog_get_post_handler}}, \
-    
-extern eSTR EWEBLOG_STR;
 
+// Nuevas definiciones
+#define EWEBLOG_CHECK_INIT() do { \
+    if (!eweblog_is_initialized) { \
+        eweblog_init(EWEBLOG_DEFAULT_CAPACITY); \
+        eweblog_is_initialized = true; \
+    } \
+} while(0)
+
+extern eSTR EWEBLOG_STR;
+extern bool eweblog_is_initialized;  // Nueva variable global
 
 void eweblog_init(unsigned CAPACITY);
 void eweblog_realocate(unsigned CAPACITY);
@@ -17,5 +24,7 @@ void eweblog_clear();
 void eweblog_free();
 void eweblog_add_str(eSTR* str);
 void eweblog_add_buff(char* buff, unsigned len);
+
+void eweblog_add_format(const char *format, ...);
 
 esp_err_t eweblog_get_post_handler(httpd_req_t *req);
